8eead0a15a2d90683899a4d583fac719d8d3fead
juliuschris <julius_chris@apps.ipb.ac.id>
2020-07-12 16:15:33 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/styles.scss'
+import DefaultLayout from "~/layouts/Default.vue";
+import "~/assets/css/bootstrap.min.css";
+import "~/assets/css/slicknav.min.css";
+import "~/assets/css/style.css";
+// import "~/assets/styles.scss";
+import BootstrapVue from "bootstrap-vue";
 
-export default function (Vue, { router, head, isClient }) {
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
+  Vue.use(BootstrapVue);
 }
