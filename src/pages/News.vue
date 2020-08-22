069a9a9040d9687fdb11c4bce96853f315d4102e
juliuschris <julius_chris@apps.ipb.ac.id>
2020-08-22 17:42:58 +0700

    cms

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -2,15 +2,13 @@
   <Layout>
     <div class="container">
       <div v-for="post in $page.allBlogPost.edges" :key="post.node.id">
-        <div
-          v-if="post.node.title!='Profil Desa'"
-        >
+        <div v-if="post.node.title!='Profil Desa' && post.node.title!='RS Rujukan'">
           <a :href="post.node.path">
             <div class="card">
-              <g-image src="../assets/logo.png" style="max-width: 50px" class="image"></g-image>
               <div class="flex-item">
                 <h2>{{ post.node.title}}</h2>
                 <p class="sub-text">Diterbitkan {{post.node.date}}</p>
+              <!--<g-image src="../assets/logo.png" style="max-width: 50px" class="image"></g-image>-->
               </div>
             </div>
           </a>
@@ -41,22 +39,22 @@ query{
 
 <style scoped>
 .sub-text {
-  font-size: 18px;
+  font-size: 14px;
 }
 .card {
-  padding: 16px;
+  padding: 5px;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
-  margin: 16px 180px;
+  justify-content: left;
+  margin: 12px 100px;
 }
 .flex-item {
   margin-left: 30px;
 }
-@media screen and (max-width: 420px) {
+@media screen and (max-width: 240px) {
   .card {
-    margin: 12px;
+    margin: 5px;
   }
 }
 </style>
